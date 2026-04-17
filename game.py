46a6673da472592ba5e9f2a302d46c5a6e4c2015
Taibi EL Yakouti <111664715+xDweeb@users.noreply.github.com>
2026-04-17 15:49:21 +0000

    Create map manager structure

--- game.py
+++ game.py
@@ -1,14 +1,9 @@
 from direct.showbase.ShowBase import ShowBase
-
+from mapmanager import Mapmanager
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-
-        self.model = loader.loadModel("models/environment")
-        self.model.reparentTo(render)
-        self.model.setScale(0.25, 0.25, 0.25)
-        self.model.setPos(-8, 42, 0)    
-        base.camera.setPos(0, -20, 3)
+        self.land = Mapmanager()
         base.camLens.setFov(90)
 base = Game()
 base.run()
---
+++ mapmanager.py
@@ -0,0 +1,5 @@
+class Mapmanager:
+    def __init__(self):
+        self.model = 'blocky.egg'
+        self.texture = 'block.png'
+        self.color = (0.2, 0.2, 0.25, 1)
