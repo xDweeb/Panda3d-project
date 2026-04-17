acbcc4821309bd2be73a1754ccea847631aa685e
Taibi EL Yakouti <111664715+xDweeb@users.noreply.github.com>
2026-04-17 15:30:44 +0000

    load enviroment and adjust camer

--- game.py
+++ game.py
@@ -9,5 +9,6 @@ class Game(ShowBase):
         self.model.setScale(0.25, 0.25, 0.25)
         self.model.setPos(-8, 42, 0)    
         base.camera.setPos(0, -20, 3)
-base = ShowBase()
+        base.camLens.setFov(90)
+base = Game()
 base.run()
