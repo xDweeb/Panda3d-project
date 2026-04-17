0613775bc19eee5cd01127b55d2df7635704d989
Taibi EL Yakouti <111664715+xDweeb@users.noreply.github.com>
2026-04-17 15:13:51 +0000

    add model and pos

--- game.py
+++ game.py
@@ -3,5 +3,11 @@ from direct.showbase.ShowBase import ShowBase
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+
+        self.model = loader.loadModel("models/environment")
+        self.model.reparentTo(render)
+        self.model.setScale(0.25, 0.25, 0.25)
+        self.model.setPos(-8, 42, 0)    
+        base.camera.setPos(0, -20, 3)
 base = ShowBase()
 base.run()
