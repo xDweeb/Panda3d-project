6643d87aaf45e9b300096249f350dc686a66c395
Taibi EL Yakouti <111664715+xDweeb@users.noreply.github.com>
2026-04-17 15:58:55 +0000

    last save

--- mapmanager.py
+++ mapmanager.py
@@ -3,3 +3,5 @@ class Mapmanager:
         self.model = 'blocky.egg'
         self.texture = 'block.png'
         self.color = (0.2, 0.2, 0.25, 1)
+        self.startNew(self);
+        self.addBlock(0, 10, 0)
