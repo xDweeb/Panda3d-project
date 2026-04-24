eb2f76572a71a762e403db36f09f4ad9218bc922
xdweb <xdweb.elyakouti@gmail.com>
2026-04-24 15:58:18 +0100

    setup my project

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from direct.showbase.ShowBase import ShowBase
-from mapmanager import Mapmanager
+
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.land = Mapmanager()
-        base.camLens.setFov(90)
+
 game = Game()
 game.run()
--- mapmanager.py
+++ mapmanager.py
@@ -1,18 +1,14 @@
 class Mapmanager:
     def __init__(self):
-        self.model = 'blocky.egg'
-        self.texture = 'block.png'
-        self.color = (0.2, 0.2, 0.25, 1)
+        self.color = (0.2, 0.8, 0.2, 1)
 
         self.startNew()
         self.addBlock((0, 10, 0))
 
     def startNew(self):
-        self.land = render.attachNewNode('land')
+        self.land = render.attachNewNode("Land")
 
     def addBlock(self, position):
-        self.block = loader.loadModel(self.model)
-        self.block.setTexture(loader.loadTexture(self.texture))
+        self.block = loader.loadModel("models/box")
         self.block.setPos(position)
-        self.block.setColor(self.color)
-        self.block.reparentTo(self.land)
+        self.block.setScale(1)
