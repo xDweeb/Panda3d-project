1d79e2a31b46d4599f295043960195c92b5283a6
xdweb <xdweb.elyakouti@gmail.com>
2026-04-24 16:20:50 +0100

    adjust camera view for block map

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from direct.showbase.ShowBase import ShowBase
 from mapmanager import Mapmanager
+
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+
         self.land = Mapmanager()
+
+        base.camera.setPos(2, -12, 8)
+        base.camera.setHpr(0, -25, 0)
         base.camLens.setFov(90)
 
 game = Game()
